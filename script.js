615fbd32ac0f5fa0aa77a624d95add521830efb8
iKiwo <urdaneta7834@gmail.com>
2025-04-02 14:52:43 -0500

    updated all

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.download-btn').forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        alert('¡El enlace de descarga estará disponible pronto!');
-    });
-});
